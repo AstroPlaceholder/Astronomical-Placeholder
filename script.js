10dd012d2a7d4aa0353cb1f305dc9db7f9d1ed27
Samuel Pickering <samuelpickering21@gmail.com>
2023-08-25 15:08:11 -0400

    Each POTD has a card shown with image and title

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ let startDate = moment().subtract(daysBack, "days") // get date from 2 weeks ago
 const startRange = startDate.format("YYYY-MM-DD"); // Format as 'YYYY-MM-DD'
 console.log("currentDate: " + currentDate)
 console.log("starting date: " + startRange)
+const gallery = document.querySelector("#gallery");
+
+function AppendPhotos (photos) {
+    for(let i = 0; i < photos.length; i++){
+        const pictureCard = document.createElement("div") // create a div
+        const pictureTitle = document.createElement("h2"); 
+        const pictureImg = document.createElement("img");
+        pictureTitle.textContent = photos[i].title // change the text of the h2 tag to the title property of the photo
+        pictureImg.src = photos[i].hdurl // set image source url of the photo in the hdurl property
+        pictureCard.append(pictureImg, pictureTitle) // add the image element and h2 element to the div
+        gallery.append(pictureCard) // add the div to the gallery
+    }
+}
+
+
+const fetchPhotos = async (url) => {
+    const response = await fetch(url) // get data from Nasa
+    console.log(response)
+    const photos = await response.json() // convert the body of the response to an array of photos 
+    console.log('data:', photos);
+    AppendPhotos(photos)
+}
 
 
 const testfetch = async (url) => {
