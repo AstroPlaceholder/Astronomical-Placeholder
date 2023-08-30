db2a2719a0857a24f62e0b4fffe7df6f34eca1c2
Samuel Pickering <samuelpickering21@gmail.com>
2023-08-30 14:53:30 -0400

    three js background implementation.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 const dateNow = moment(); // use library to get current data
 const currentDate = dateNow.format("YYYY-MM-DD"); // Format as 'YYYY-MM-DD'
 let daysBack = 14;
@@ -16,6 +18,115 @@ const closeModalButton = document.querySelector("#closeModal");
 let obj = {
   date: "454454",
 };
+const container = document.querySelector(".background")
+const loader = new  THREE.TextureLoader()
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000)
+const particlesGeometry = new THREE.BufferGeometry();
+const counts = 3000
+
+const positions = new Float32Array(counts * 3)
+
+for (let i = 0; i < counts * 3; i++){
+  positions[i] = (Math.random() - 0.5) * 10
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positions, 3)
+)
+
+const particlesMaterial = new THREE.PointsMaterial()
+particlesMaterial.size = 0.01
+particlesMaterial.sizeAttenuation = true
+
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight)
+function resizeRenderer() {
+  const newWidth = window.innerWidth;
+  const newHeight = window.innerHeight;
+
+
+  renderer.setSize(newWidth, newHeight);
+
+ 
+  camera.aspect = newWidth / newHeight;
+  camera.updateProjectionMatrix();
+}
+
+
+window.addEventListener("resize", resizeRenderer);
+
+// Initial call to set up the renderer and camera sizes
+resizeRenderer();
+
+
+
+
+
+
+const canvas = renderer.domElement;
+canvas.classList.add("background-canvas");
+container.appendChild(canvas);
+
+
+const geometry = new THREE.PlaneGeometry(6, 6, 25, 5);
+const material = new THREE.MeshBasicMaterial({
+  // color: 0xff0000,
+  map: loader.load("bg2.png")
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+camera.position.z = 5
+
+const count = geometry.attributes.position.count;
+const clock = new THREE.Clock();
+
+
+function animate() {
+  const time = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime()
+
+  particles.position.y = -elapsedTime * 0.02;
+
+  for(let i = 0; i < count; i++){
+    const x = geometry.attributes.position.getX(i)
+    const y = geometry.attributes.position.getY(i)
+
+    geometry.attributes.position.setZ(i, -y * time * 0.3)
+    geometry.computeVertexNormals()
+    geometry.attributes.position.needsUpdate = true
+  }
+  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+}
+// function animate() {
+//   const time = clock.getElapsedTime();
+
+//   particles.geometry.attributes.position.array.forEach((_, index) => {
+//     const x = positions[index * 3];
+//     const y = positions[index * 3 + 1];
+//     const z = positions[index * 3 + 2];
+//     particles.position.y = -elapsedTime * 0.02;
+    
+//     // Update the z-coordinate of the particles based on the time
+//     positions[index * 3 + 2] = z - y * time * 0.3;
+//   });
+
+//   particles.geometry.attributes.position.needsUpdate = true;
+
+//   requestAnimationFrame(animate);
+//   renderer.render(scene, camera);
+// }
+
+animate()
+
+
+
 
 function createGalleryItem(photo) {
   const pictureCard = document.createElement("div");
